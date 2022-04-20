328d639f645459f2d00b6af41fb5b15a396f36a9
Your Name <moisesjuniorcas20@gmail.com>
2022-04-19 20:55:57 -0500

    clase 1

---
+++ Clase1/clase1.py
@@ -0,0 +1,11 @@
+
+cafe = 0 
+taza = 1 
+agua = 1 
+if (cafe==1  & taza==1 & agua==1): 
+    print("aca esta tu taza de cafe") 
+else: 
+    print("faltan ingredientes")
+
+#REPASAMOS QUE ES UN ALGORITMO Y SE HIZO EL EJERCICIO DE PROPONER UN EJERCICIO DE PREPARAR UNA TAZA DE CAFÉ 
+
