fd49c6ea94daf6522ca1aa51bd4dd3f649d3061a
Your Name <moisesjuniorcas20@gmail.com>
2022-05-02 20:41:47 -0500

    se agrega ciclo a modo de prueba

--- Reto1/reto1.py
+++ Reto1/reto1.py
@@ -1,4 +1,3 @@
-from ast import For
 
 
 def calcularnota(n1,n2,n3,p1,p2,p3): #definimos la función con los parametros que va a recibir
@@ -11,9 +10,11 @@ n2=float(input("escriba la nota 2: "))
 p2=float(input("escriba promedio nota 2, por ejemplo si es 30%, escribe 0.30: "))
 n3=float(input("escriba la nota 3: "))
 p3=float(input("escriba promedio nota 3, por ejemplo si es 15%, escribe 0.15: "))
-sumapromedio=p1+p2+p3 #sumamos los promedios
+sumapromedio=(p1+p2+p3) #sumamos los promedios
 
-notafinal=calcularnota(n1,n2,n3,p1,p2,p3) #invocamos la función con los parametros que le pasamos, recibidos en el input
-print("Hola ",nombre,"La nota final es: ", notafinal)#mostramos el resultado al usuario 
+while sumapromedio == 1: #si la suma de los promedios es 1  
+     #mientras la suma de los promedios sea 1
+    notafinal=calcularnota(n1,n2,n3,p1,p2,p3) #invocamos la función con los parametros que le pasamos, recibidos en el input
+    print("Hola ",nombre,"La nota final es: ", notafinal)#mostramos el resultado al usuario 
 
 
