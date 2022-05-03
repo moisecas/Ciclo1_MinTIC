af13e0e6584254e345275de67c784a6370224684
Your Name <moisesjuniorcas20@gmail.com>
2022-05-03 18:12:37 -0500

    ejercicio

--- clase4/ejercicio.py
+++ clase4/ejercicio.py
@@ -13,3 +13,17 @@ elif peso_nico >=41 and peso_nico<=80:
 else:
   print("Nico,esta en tapa 4") 
   print("el peso de nico es: ", peso_nico) 
+
+#solución dos
+ale=int(input("Digite el peso de Ale: "))
+gil=(ale*2)+4
+nico=int((ale+gil)/5)
+
+print("El peso de Ale es: ", ale, "El peso de Gil es: ", gil, "el peso de Nico es: ", nico)
+
+if nico>=20:
+  print("Nico se encuentra en etapa 1 y su peso es de: ", nico)
+elif nico >=21 and nico <= 40:
+  print("Nico se encuentra en etapa 2 y su peso es de: ", nico)
+else:
+  print("Nico se encuentra en etapa 3 y su peso es de: ", nico)   
