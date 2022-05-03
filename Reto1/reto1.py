47904e3a25a63789bb91428f6b80023e5b438dd5
Your Name <moisesjuniorcas20@gmail.com>
2022-05-02 21:34:25 -0500

    se agrega ciclo while para validar el porcentaje total de cada nota

--- Reto1/reto1.py
+++ Reto1/reto1.py
@@ -1,5 +1,7 @@
 
 
+
+
 def calcularnota(n1,n2,n3,p1,p2,p3): #definimos la función con los parametros que va a recibir
     return (n1*p1)+(n2*p2)+(n3*p3)
 
@@ -13,8 +15,18 @@ p3=float(input("escriba promedio nota 3, por ejemplo si es 15%, escribe 0.15: ")
 sumapromedio=(p1+p2+p3) #sumamos los promedios
 
 while sumapromedio == 1: #si la suma de los promedios es 1  
-     #mientras la suma de los promedios sea 1
+    print("Debe volver a escribir las notas, asegurese de digitar bien los porcentajes de cada nota")
+    calcularnota(n1,n2,n3,p1,p2,p3) #volvemos a llamar a la función
+    n1=float(input("escriba la nota 1: "))
+    p1=float(input("escriba promedio nota 1, por ejemplo si es 20%, escribe 0.20: "))
+    n2=float(input("escriba la nota 2: "))
+    p2=float(input("escriba promedio nota 2, por ejemplo si es 30%, escribe 0.30: "))
+    n3=float(input("escriba la nota 3: "))
+    p3=float(input("escriba promedio nota 3, por ejemplo si es 15%, escribe 0.15: "))
+    sumapromedio=(p1+p2+p3) #sumamos los promedios nuevamente 
+
     notafinal=calcularnota(n1,n2,n3,p1,p2,p3) #invocamos la función con los parametros que le pasamos, recibidos en el input
-    print("Hola ",nombre,"La nota final es: ", notafinal)#mostramos el resultado al usuario 
+   
+print("Hola ",nombre,"La nota final es: ", notafinal)#mostramos el resultado al usuario 
 
 
