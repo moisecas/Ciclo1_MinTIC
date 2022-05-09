f442396cc057943efc5f237c388cc9c8b18c7905
Your Name <moisesjuniorcas20@gmail.com>
2022-05-09 07:43:07 -0500

    se agregan mas intentos de solución al reto1

--- Reto1/reto1_4.py
+++ Reto1/reto1_4.py
@@ -1,41 +1,33 @@
 print("¡Bienvenido! En esta aplicación los estudiantes podrán gestionar las notas de su materia.")
-
 nombre=input("Por favor ingrese su nombre: ")
 materia=input("Ingrese el nombre de la materia: ") #solicitamos el nombre de la materia
-
-notas=float(input("Ingrese la nota obtenida: ")) #solictamos al usuario las notas
-promedio=int(input("Ingrese el porcentaje de la nota: "))
-validar=input("¿Falta añadir notas? S/N: ") 
-
 sumapromedio=0
-sumapromedio=promedio+sumapromedio
 nota=0
-nota=nota+notas
-
-
 aprobado=("Aprobado")
 noaprobado=("No aprobado")
 validar="S"
+contador=0
+contador2=0
 
 
-while sumapromedio<promedio:
+while contador < 100 and validar=="S":
     notas=float(input("Ingrese la nota obtenida: ")) #solictamos al usuario las notas
     promedio=int(input("Ingrese el porcentaje de la nota: "))
-    validar=input("¿Falta añadir notas? S/N: ") 
-    promedio=sumapromedio+sumapromedio
-    sumapromedio+=1
+    validar=input("¿Falta añadir notas? S/N: ")     
+    sumapromedio=promedio+contador
+    nota=contador2+notas
+    
+    
     
     if sumapromedio==100 or validar=="N":
-        nota_final=nota*(sumapromedio/100) 
+        nota_final=nota*(sumapromedio/100)
+        if nota_final<3:
         
-
-    if nota_final<3:
-        
-        print(f"El Estudiante {nombre} cursó la materia {materia} y obtuvo {nota_final} resultando en no {aprobado}.")  
+          print(f"El Estudiante {nombre} cursó la materia {materia} y obtuvo {nota_final} resultando en no {aprobado}.")  
+                  
                                     
     elif nota_final>=3:
         print(f"El Estudiante {nombre} cursó la materia {materia} y obtuvo {nota_final} resultando en {aprobado}.") 
-
 
 
         
